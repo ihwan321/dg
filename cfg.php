3eef55f19bcab093006dafa3c9088158fb95ba98
ihwan321 <63368199+ihwan321@users.noreply.github.com>
2020-04-16 03:39:55 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$token = "cookie: DOGEToken=wGUC5FyUJFdjN7PywLLHY3KHL50SJok4";
+$cookie = "cookie: _gat_gtag_UA_158667053_2=1";
+$cookie1 = "cookie: __ga=GA1.2.1983932437.1586978582";
+$cookie2 = "cookie: _gid=GA1.2.963696121.1586978582";
+$useragent = "Mozilla/5.0 (Linux; Android 9; CPH1937) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36";
+$phpsessid = "cookie: PHPSESSID=15f06f642eb94c6fac75afc6dd9545aa";
+
+
+
+
+?>
